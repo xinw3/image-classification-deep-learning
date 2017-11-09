0ff9d6e8931f396e8b592dd244cf7166d29fbf86
TengZhong <teng.zhong@outlook.com>
2017-11-09 08:46:29 -0500

    update keras folder

---
+++ Kaggle-keras/params.py
@@ -0,0 +1,7 @@
+base_dir = '/Users/zhongteng/Desktop/0_10707_FinalProject/707_project/Kaggle-Cdiscount-Image-Classification-Challenge'  # Change to your base folder
+
+height = 180
+width = 180
+batch_size = 32
+max_epochs = 5
+base_model = 'vgg16'
