55ac9d2f5dda4178e9d925072697f46bbd38b635
Xin Wang <wangxin94@163.com>
2017-12-02 20:25:38 -0500

    change input file path

--- Kaggle-keras/params.py
+++ Kaggle-keras/params.py
@@ -1,5 +1,5 @@
-base_dir = '/Users/xinwang/Desktop/707_project/Kaggle-keras'  # Change to your base folder
-
+# base_dir = '/Users/xinwang/Desktop/707_project/Kaggle-keras'  # Change to your base folder
+base_dir = '/home/xinw1994/707project/git_repo/Kaggle-keras'
 height = 180
 width = 180
 batch_size = 32
--- Kaggle-keras/read_data_train.py
+++ Kaggle-keras/read_data_train.py
@@ -10,7 +10,7 @@ import params
 
 base_dir = params.base_dir
 
-bson_file = 'input/train_example.bson'
+bson_file = '~/707project/extracted_data/all_classes_train.bson'
 image_dir = 'train_images'
 image_path = os.path.join(base_dir, image_dir)
 bson_file_path = os.path.join(base_dir, bson_file)
